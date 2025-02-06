fbddce3215479c436305ac6ac19c6394c8b3f625
Conquerer-yinn <48711400+Conquerer-yinn@users.noreply.github.com>
2025-02-06 17:25:05 +0800

    feat(config): 接入环境变量，加入 TUSHARE_TOKEN

--- common/config.py
+++ common/config.py
@@ -3,6 +3,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# API configuration
+TUSHARE_TOKEN = os.getenv("TUSHARE_TOKEN")
+
 # Output configuration
 MASTER_DATA_DIR = os.path.join(BASE_DIR, "storage", "data_master")
 BACKUP_DIR = os.path.join(BASE_DIR, "storage", "backups")
