43fdd0956750fb65489a255f8b0bdcde395b9430
Conquerer-yinn <48711400+Conquerer-yinn@users.noreply.github.com>
2025-03-25 16:02:58 +0800

    feat(indicators): 高度观察骨架

---
+++ market/indicators/sentiment_height.py
@@ -0,0 +1,17 @@
+﻿from datetime import timedelta
+
+import pandas as pd
+
+
+HEIGHT_OBSERVATION_COLUMNS = [
+    "日期",
+    "全市场近十日高度(%)",
+    "全市场高度个股",
+    "主板近十日高度(%)",
+    "主板高度个股",
+    "创业板近十日高度(%)",
+    "创业板高度个股",
+    "最高连板",
+]
+
+
