a3e3e5acfe730280ac6276f2022043628e976266
Conquerer-yinn <48711400+Conquerer-yinn@users.noreply.github.com>
2025-03-29 17:10:50 +0800

    feat(indicators): 创业板专区骨架

---
+++ market/indicators/sentiment_chinext.py
@@ -0,0 +1,22 @@
+﻿import pandas as pd
+
+from market.indicators.sentiment_market import count_broken_limit, count_large_retrace
+
+
+CHINEXT_COLUMNS = [
+    "日期",
+    "创业板成交额(亿元)",
+    "创业板成交额占全市场比重(%)",
+    "创业板涨停数",
+    "创业板炸板数",
+    "创业板大回撤数",
+    "创业板连板高度",
+    "创业板最高板个股",
+    "创业板涨停股平均成交额(亿元)",
+    "创业板涨停股平均换手率(%)",
+    "创业板涨停股成交额总额(亿元)",
+    "创业板最大成交涨停股",
+    "创业板最大成交涨停股成交额(亿元)",
+]
+
+
